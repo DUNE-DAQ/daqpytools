733a2c95e308e789d6ace94bebe6d8343915787c
Emir Muhammad <emir.muhammad@cern.ch>
2026-04-01 17:10:54 +0200

    Add skeleton of generate_uml script

---
+++ src/daqpytools/apps/generate_uml.py
@@ -0,0 +1,16 @@
+"""
+
+This should be code that generates uml diagrams
+
+eg for now it will be basically 
+
+daqpytools-generate-uml --project daqpytools --concise etc 
+"""
+
+
+
+def main():
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
